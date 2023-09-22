b36d4cb776c7aa9e28fc234dc2699d1f9da7a015
andrsand <angorko@list.ru>
2023-09-22 06:50:19 +0300

    Start TS

---
+++ 1types.js
@@ -0,0 +1,11 @@
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello';
+var numberArray = [1, 2, 3];
+var numberArray2 = [1, 2, 3];
+var words = ['Hello', 'TypeScript'];
+// Tuple
+var contact = ['Alex', 9987623333];
---
+++ 1types.ts
@@ -0,0 +1,16 @@
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const message: string = 'Hello'
+
+const numberArray: number[] = [1, 2, 3]
+const numberArray2: Array<number> = [1, 2, 3]
+
+const words: string[] = ['Hello', 'TypeScript']
+
+// Tuple
+const contact: [string, number] = ['Alex', 9987623333]
