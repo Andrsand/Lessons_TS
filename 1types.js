abcf7088864adcd2551c3c21297e4134c4408df3
andrsand <angorko@list.ru>
2023-09-22 08:08:55 +0300

    example type any

--- 1types.js
+++ 1types.js
@@ -1,6 +1,6 @@
 var isFetching = true;
 var isLoading = false;
-var int = 42;
+var intel = 42;
 var float = 4.2;
 var num = 3e10;
 var message = 'Hello';
@@ -9,3 +9,7 @@ var numberArray2 = [1, 2, 3];
 var words = ['Hello', 'TypeScript'];
 // Tuple
 var contact = ['Alex', 9987623333];
+// Any
+var variable = 42;
+variable = 'ccc';
+//# sourceMappingURL=1types.js.map
--- 1types.ts
+++ 1types.ts
@@ -1,16 +1,26 @@
-const isFetching: boolean = true
-const isLoading: boolean = false
 
-const int: number = 42
-const float: number = 4.2
-const num: number = 3e10
 
-const message: string = 'Hello'
+const isFetching: boolean = true;
+const isLoading: boolean = false;
 
-const numberArray: number[] = [1, 2, 3]
-const numberArray2: Array<number> = [1, 2, 3]
+const intel: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
 
-const words: string[] = ['Hello', 'TypeScript']
+const message: string = 'Hello';
+
+const numberArray: number[] = [1, 2, 3];
+const numberArray2: Array<number> = [1, 2, 3];
+
+const words: string[] = ['Hello', 'TypeScript'];
 
 // Tuple
-const contact: [string, number] = ['Alex', 9987623333]
+const contact: [string, number] = ['Alex', 9987623333];
+
+// Any
+let variable: any = 42;
+
+
+variable = 'ccc';
+
+
